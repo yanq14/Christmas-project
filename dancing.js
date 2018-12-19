46c1936a40e8aa80be0d10dd9c1a30f635250911
yanq14 <30380969+yanq14@users.noreply.github.com>
2018-12-19 16:41:50 +0800

    Update dancing.js

--- dancing.js
+++ dancing.js
@@ -58,3 +58,46 @@ $(function() {
     //圣诞主题效果，开始
     Christmas()
 })
+
+/**
+ * 慕课网特制
+ * 圣诞主题效果
+ * @type {Object}
+ */
+
+
+/**
+ * 背景音乐
+ * @param {[type]} url  [description]
+ * @param {[type]} loop [description]
+ */
+function HTML5Audio(url, loop) {
+    var audio = new Audio(url);
+    audio.autoplay = true;
+    audio.loop = loop || false; //是否循环
+    audio.play();
+    return {
+        end: function(callback) {
+            audio.addEventListener('ended', function() {
+                callback()
+            }, false);
+        }
+    }
+}
+
+
+$(function() {
+    $("button:first").click(function() {
+        //背景音乐
+        var audio1 = HTML5Audio('http://www.imooc.com/upload/media/one.mp3')
+        audio1.end(function() {
+            alert("音乐结束")
+        })
+    })
+    $("button:last").click(function() {
+        var audio2= new Audio('http://www.imooc.com/upload/media/two.mp3')
+        audio.loop=loop;
+        audio.play();
+    })
+})
+
